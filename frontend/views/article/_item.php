67d0a8a56473464e76cc10577e06e9d76d0ca45e
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2016-12-28 19:42:53 +0400

    Required omcrn/yii2-base.

--- frontend/views/article/_item.php
+++ frontend/views/article/_item.php
@@ -25,17 +25,17 @@ use yii\helpers\Html;
         </div>
         <div class="article-content">
             <?php if ($model->thumbnail_path): ?>
-                <?php echo Html::img(
+                <?php echo Html::a(Html::img(
                     Yii::$app->glide->createSignedUrl([
                         'glide/index',
                         'path' => $model->thumbnail_path,
                         'w' => 100
                     ], true),
                     ['class' => 'article-thumb img-rounded pull-left']
-                ) ?>
+                ), \yii\helpers\Url::to(['view', 'slug' => $model->slug])) ?>
             <?php endif; ?>
             <div class="article-text">
-                <?php echo \yii\helpers\StringHelper::truncate($model->body, 150, '...', null, true) ?>
+                <?php echo \yii\helpers\StringHelper::truncate($model->body, 250, '...', null, true) ?>
             </div>
         </div>
     </div>
--- frontend/views/article/index.php
+++ frontend/views/article/index.php
@@ -1,14 +1,17 @@
 <?php
+
 /* @var $this yii\web\View */
+/* @var $dataProvider \yii\data\ActiveDataProvider */
+
 $this->title = Yii::t('frontend', 'Articles')
 ?>
 <div id="article-index">
     <h1><?php echo Yii::t('frontend', 'Articles') ?></h1>
     <?php echo \yii\widgets\ListView::widget([
-        'dataProvider'=>$dataProvider,
-        'pager'=>[
-            'hideOnSinglePage'=>true,
+        'dataProvider' => $dataProvider,
+        'pager' => [
+            'hideOnSinglePage' => true,
         ],
-        'itemView'=>'_item'
-    ])?>
+        'itemView' => '_item'
+    ]) ?>
 </div>
